4c75898b01a6f08cdfde494aed7c088f278f4ef3
Krystian Woźniak <krys.blu@gmail.com>
2019-03-14 11:43:56 +0100

    Edited DonationDto

--- src/main/java/pl/blu911/oddam/domain/dto/DonationDto.java
+++ src/main/java/pl/blu911/oddam/domain/dto/DonationDto.java
@@ -1,6 +1,7 @@
 package pl.blu911.oddam.domain.dto;
 
 import lombok.Data;
+import lombok.ToString;
 import pl.blu911.oddam.domain.Address;
 import pl.blu911.oddam.domain.Category;
 import pl.blu911.oddam.domain.User;
@@ -10,6 +11,7 @@ import java.time.LocalTime;
 import java.util.List;
 
 @Data
+@ToString(exclude = {"userDetails", "institutionDetails", "whatToDonate", "pickUpAddress"})
 public class DonationDto {
 
     private List<Category> whatToDonate;
